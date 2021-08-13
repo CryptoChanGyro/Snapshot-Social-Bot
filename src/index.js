1806756c37c5197d375f9d39a510d6ff0b6ac578
Mekyle Logen Naidoo <88673807+CryptoChanGyro@users.noreply.github.com>
2021-08-13 10:48:51 +0200

    Creating the endpoint

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-const fetch = require('node-fetch');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const query = `
-query Votes {
-    votes (
-      first: 1000
-      skip: 0
-      where: {
-        proposal: "QmPvbwguLfcVryzBRrbY4Pb9bCtxURagdv1XjhtFLf3wHj"
-      }
-      orderBy: "created",
-      orderDirection: desc
-    ) {
-      id
-      voter
-      created
-      proposal {
-        id
-      }
-      choice
-      space {
-        id
-      }
-    }
-  }
-`;
-const url = "https://hub.snapshot.page/graphql";
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query })
+const TGtoken = ""; // Add your bot token that you get from the bot father on Telegram
+const telegramUrl = "https://api.telegram.org/bot" + TGtoken;
+
+// The event data we're posting to snapshot
+const proposalID = ""; // This is the proposal ID we need
+const snapshotSpace = ""; // This is the snapshot space you want to get data from (eg: yam.eth)
+const snapshotExpire = ""; // Expiry something idk
+const snapshotEvent = ""; // This is the event type we want
+
+const telegramWebdata = {
+  id: proposalID,
+  event: "proposal/created",
+  space: snapshotSpace,
+  expire: snapshotExpire
 };
-fetch(url, opts)
-  .then(res => res.json())
-  .then(console.log)
-  .catch(console.error);
+
+app.post('/api/telegram', function (req, res) {
+    req.send('')
+    res.send('Got a POST request')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+getVotes()
